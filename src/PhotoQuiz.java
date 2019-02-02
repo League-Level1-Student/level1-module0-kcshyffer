34fbe7b8af284306b0b6dd80a69fe4302ed149cf
kcshyffer <League@AdministorsiMac.attlocal.net>
2019-02-02 11:27:21 -0800

    Done with Half of Photo quiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,21 +22,25 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+                String image = ("https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Solid_blue.svg/225px-Solid_blue.svg.png");
 		// 2. create a variable of type "Component" that will hold your image
-
+                Component c;
 		// 3. use the "createImage()" method below to initialize your Component
-
+                c = createImage(image);
 		// 4. add the image to the quiz window
-
+                quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
-
+                pack(quizWindow);
 		// 6. ask a question that relates to the image
-
+               String a = JOptionPane.showInputDialog("What color is the image?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+                if (a.equalsIgnoreCase("blue")) {
+					JOptionPane.showMessageDialog(null, "CORRECT!");
+				}
 		// 8. print "INCORRECT" if the answer is wrong
-
+                else {
+                		JOptionPane.showMessageDialog(null, "INCORRECT!");
+                }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
@@ -51,6 +55,11 @@ public class PhotoQuiz {
 
 	}
 
+	private static void pack(JFrame quizWindow) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	private static Component createImage(String imageUrl) throws MalformedURLException {
 		URL url = new URL(imageUrl);
 		Icon icon = new ImageIcon(url);
@@ -62,8 +71,3 @@ public class PhotoQuiz {
 	// *14. add scoring to your quiz
 	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
 }
-
-
-
-
-
