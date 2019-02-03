ed4e946a0b580db0e11599b81de9bbb0e31c154f
League <league@WTS9.attlocal.net>
2019-02-03 14:24:13 -0800

    Finished Photo Quiz/ Started Drum Kit

--- src/DrumKit.java
+++ src/DrumKit.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) The League of Amazing Programmers 2013-2017
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
  */
 
@@ -28,30 +28,32 @@ public class DrumKit implements MouseListener {
 
     private void getGoing() throws MalformedURLException {
    	 // 1. Make a JFrame variable and initialize it using "new JFrame()"
-   	 
+    		JFrame j = new JFrame();
+    		JPanel p = new JPanel();
+   	 // 6. Add the panel to the frame. (The panel is invisible.)
+    		j.add(p);
    	 // 2. Make the frame visible and 
     //     set its default close operation to JFrame.EXIT_ON_CLOSE
-   	 
+    		j.setVisible(true);
+    		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    	 // 3. Set the size of the frame
-   	 
+    		//j.setSize(500, 500);
    	 // 4. Set the title of the frame
-   	 
+    		j.setTitle("Drum Kit");
    	 // 5. Make a JPanel variable and initialize it using "new JPanel().
-   	 
-   	 // 6. Add the panel to the frame. (The panel is invisible.)
-    
+    		
    	 // 7. Download an image of a drum from the Internet. Drop it into your Eclipse project under "default package".
    	 // 8. Put the name of your image in a String variable.
-    
+    		String image = "snare.jpg";
    	 // 9. Edit the next line to use your String variable
-// drumLabelWithImage = createLabelImage(drumImageString);
-   	 
+    		drumLabelWithImage = createLabelImage(image);
    	 // 10. Add the image to the panel
-    
+    		j.add(drumLabelWithImage);
    	 // 11. Set the layout of the panel to "new GridLayout()"
-    
+    		//p.layout();
+    		p.setLayout(new GridLayout());
       	 // 12. call the pack() method on the frame.  Run your program. Do you see your drum image?
-   	  
+    		j.pack();
    	 // 13. add this mouse listener to drumLabelWithImage
     	
      	 // 18. Add more images to make a drumkit. Remember to add this mouse listener to each one.
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) The League of Amazing Programmers 2013-2017
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
  */
 
@@ -30,7 +30,7 @@ public class PhotoQuiz {
 		// 4. add the image to the quiz window
                 quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
-                pack(quizWindow);
+                quizWindow.pack();
 		// 6. ask a question that relates to the image
                String a = JOptionPane.showInputDialog("What color is the image?");
 		// 7. print "CORRECT" if the user gave the right answer
@@ -42,24 +42,28 @@ public class PhotoQuiz {
                 		JOptionPane.showMessageDialog(null, "INCORRECT!");
                 }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+                quizWindow.remove(c);
 		// 10. find another image and create it (might take more than one line of code)
-
+                String Image = ("https://cdn11.bigcommerce.com/s-hfhomm5/images/stencil/1280x1280/products/180/451/Solid_Red_Sized__25214.1507754519.jpg?c=2&imbypass=on");
+                Component e;
+                e = createImage(Image);
 		// 11. add the second image to the quiz window
-
+                quizWindow.add(e);
 		// 12. pack the quiz window
-
+                quizWindow.pack();
 		// 13. ask another question
-
+                String b = JOptionPane.showInputDialog("What color is the image?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+                if (b.equalsIgnoreCase("red")) {
+					JOptionPane.showMessageDialog(null, "CORRECT!");
+				}
+		// 8. print "INCORRECT" if the answer is wrong
+                else {
+                		JOptionPane.showMessageDialog(null, "INCORRECT!");
+                }
 	}
 
-	private static void pack(JFrame quizWindow) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	private static Component createImage(String imageUrl) throws MalformedURLException {
 		URL url = new URL(imageUrl);
 		Icon icon = new ImageIcon(url);
