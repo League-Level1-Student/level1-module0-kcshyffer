683f33090a814d84c088cead9ed57976875ae481
kcshyffer <league@WorthyMini-6.attlocal.net>
2019-03-10 14:26:13 -0700

    Done Fortune cookie

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,4 +1,42 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class FortuneCookie {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
+public class FortuneCookie implements ActionListener {
+	
+public void showButton() {
+    JFrame bruhFrame = new JFrame();
+    bruhFrame.setVisible(true);
+    JButton bruhButton = new JButton();
+    bruhFrame.add(bruhButton);
+    bruhFrame.pack();
+    bruhButton.addActionListener(this);
+    System.out.println("Button clicked");
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("Woohoo!");
+	int rand = new Random().nextInt(5);
+	if (rand == 1) {
+		JOptionPane.showMessageDialog(null, "Today it's up to you to create the peacefulness you long for.");
+	}
+	else if (rand == 2) {
+		JOptionPane.showMessageDialog(null, "A friend asks only for your time not your money.");
+	}
+	else if (rand == 3) {
+		JOptionPane.showMessageDialog(null, "If you refuse to accept anything but the best, you very often get it.");
+	}
+	else if (rand == 4) {
+		JOptionPane.showMessageDialog(null, "A smile is your passport into the hearts of others.");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "You will game today.");
+	}
+}
 }
---
+++ src/FortuneCookieRunner.java
@@ -0,0 +1,5 @@
+public class FortuneCookieRunner {
+	   public static void main(String[] args) {
+	      new FortuneCookie().showButton();
+	   }
+	}
