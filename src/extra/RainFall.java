ae47cb63be9eabcec2f9e6f03f85dbd51e8e31e9
kcshyffer <league@iMac-2.attlocal.net>
2019-04-28 17:27:12 -0700

    BruhSnard

--- src/extra/RainFall.java
+++ src/extra/RainFall.java
@@ -2,20 +2,26 @@ void setup(){
   size(500, 500);
   background(50,23,100);
 };
+int xValue = (int)random(20,230);
 
 void draw(){
   rainCreate();
+  fill(50,23,100);
+  rect(0,0,500,500);
+  int xxValue = (int)random(20,480);
   fill(0, 85, 255);
-stroke(0, 0, 0);
-ellipse(xValue, 0, 20, 35);
-
+  stroke(0, 0, 0);
+  rainFall(xxValue);
 };
 
 void rainCreate(){
-   int xValue = (int)random(20,230);
+   
 };
 
 
 void rainFall(){
-  
+  fill(50,23,100);
+  rect(0,0,500,500);
+  fill(0, 85, 255);
+  ellipse(xValue, 0, 20, 35);
 };
